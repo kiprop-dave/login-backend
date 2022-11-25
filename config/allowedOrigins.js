6a7e7acc0894d17b0935c3e3f1c5566e7f331a71
kiprop-dave <davekip16@gmail.com>
2022-11-25 15:41:06 +0300

    added allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const alloweOrigins = [
-  "https://www.yoursite.com",
+  "https://todo-app-fullstack-azure.vercel.app",
+  "https://todo-app-fullstack-git-main-kiprop-dave.vercel.app/",
+  "https://todo-app-fullstack-kiprop-dave.vercel.app/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
-  "https://leetcode.com",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:3000",
